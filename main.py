8c8b8a8da2866ba21eeab0879aac898d7813927a
Mustafa Dede <52383970+mustafadede@users.noreply.github.com>
2019-09-20 08:43:00 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+from student import *
+import time
+print("""
+WELCOME !
+PLEASE CHOOSE:
+1 - QUIT
+2 - ADD STUDENT
+3 - UPDATE STUDENT PROPERTY
+4 - DELETE STUDENT
+5 - SHOW ALL STUDENTS
+""")
+data=DataBase()
+while True:
+    try:
+        make_decision = int(input(": "))
+        if make_decision == 1:
+            print("Please wait..")
+            time.sleep(3)
+            break
+        if make_decision == 2:
+            search_id = int(input("ID: "))
+            name = input("Name and Surname: ")
+            bed = int(input("Please give a bed number: "))
+            stud = Student(search_id,name,bed)
+            data.create_student(stud)
+        if make_decision == 3:
+            n_bed = int(input("New Bed Number: "))
+            bed = int(input("Old Bed Number: "))
+            data.update_student(n_bed,bed)
+        if make_decision == 4:
+            bed =int(input("Bed Number: "))
+            data.delete_student(bed)
+        if make_decision == 5:
+            data.all_student()
+    except:
+        print("Wrong!")
---
+++ student.py
@@ -0,0 +1,42 @@
+import sqlite3
+#STUDENT STUFF
+class Student():
+    def __init__(self,search_id=None,namesurname=None,bed_number=None):
+        self.namesurname=namesurname
+        self.bed_number=bed_number
+        self.search_id=search_id
+    def __str__(self):
+        return f"Name: {self.namesurname} \nSearch Id: {self.search_id} \nBed number: {self.bed_number}"
+#DATABASE STUFF
+class DataBase():
+    def __init__(self):
+        self.connection()
+    def connection(self):
+        self.link = sqlite3.connect("student_database.db")
+        self.cursor = self.link.cursor()
+        self.cursor.execute("Create table If not exists STUDENT (search_id integer,name_surname text,bed_number integer PRIMARY_KEY)")
+        self.link.commit()
+    def disconnection(self):
+        self.link.close()
+    #CREATE A STUDENT
+    def create_student(self,Student):
+        self.cursor.execute("Insert into STUDENT Values(?,?,?)",(Student.search_id,Student.namesurname,Student.bed_number))
+        self.link.commit()
+    #USİNG BED NUMBER FOR DELETE STUDENT
+    def delete_student(self,bed_number):
+        self.cursor.execute("Delete from STUDENT where bed_number= ?",(bed_number,))
+        self.link.commit()
+    #UPDATE STUDENT BED NUMBER
+    def update_student(self,new_bed,bed_number):
+        self.cursor.execute("Update STUDENT set bed_number= ? where bed_number = ?",(new_bed,bed_number,))
+        self.link.commit()
+    #SHOW ALL STUDENT
+    def all_student(self):
+        self.cursor.execute("Select * from STUDENT")
+        all_student = self.cursor.fetchall()
+        if all_student == 0:
+            print("None")
+        else:
+            for i in all_student:
+                students = Student(i[0],i[1],i[2])
+                print(students)
